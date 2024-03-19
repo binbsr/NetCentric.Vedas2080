3597f4ab8f69047b7af88c294cbe37925a62de98
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-03-20 02:22:27 +0545

    Add C of CRUD

--- WebDevelopment/WebApp/BookStore/Controllers/AuthorController.cs
+++ WebDevelopment/WebApp/BookStore/Controllers/AuthorController.cs
@@ -5,9 +5,24 @@ public class AuthorController: Controller
     public IActionResult Index()
     {
         BookStoreDb db = new BookStoreDb();
-
         List<Author> authors = db.Authors.ToList();
 
         return View(authors);
+    }
+
+    // [HttpGet] - default
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Author author)
+    {
+        BookStoreDb db = new BookStoreDb();
+        db.Authors.Add(author);
+        db.SaveChanges();
+
+        return RedirectToAction("Index");
     }
 }
